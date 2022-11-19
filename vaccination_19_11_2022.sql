14791cef3e7b68b66e79193d04849e23035bca2f
MaximTislenko <75487198+MaximTislenko@users.noreply.github.com>
2022-11-19 14:36:26 +0300

    Update vaccination_19_11_2022.sql

--- vaccination_19_11_2022.sql
+++ vaccination_19_11_2022.sql
@@ -4,6 +4,12 @@ CREATE TABLE vaccines (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Id_препарата",
   name_vaccine_rus VARCHAR(50) NOT NULL COMMENT "Русское именование вакцины",
   name_vaccine_eng VARCHAR(50) NOT NULL COMMENT "Английское именование вакцины",
-  id_vac_dis_obligatory_01 VARCHAR(50) NOT NULL COMMENT "Обязательный компонент вакцины_01",
+  id_vac_dis_obligatory_01 VARCHAR(50) COMMENT "Обязательный компонент вакцины_01",
+  id_vac_dis_obligatory_02 VARCHAR(50) COMMENT "Обязательный компонент вакцины_02",
+  id_vac_dis_obligatory_03 VARCHAR(50) COMMENT "Обязательный компонент вакцины_03",
+  id_vac_dis_obligatory_04 VARCHAR(50) COMMENT "Обязательный компонент вакцины_04",
+  id_vac_dis_obligatory_05 VARCHAR(50) COMMENT "Обязательный компонент вакцины_05",
+  id_vac_dis_optional_01 VARCHAR(50) COMMENT "Необязательный компонент вакцины_01",
+  id_vac_dis_optional_02 VARCHAR(50) COMMENT "Необязательный компонент вакцины_02",
   
 ) COMMENT "Вакцинные препараты";
