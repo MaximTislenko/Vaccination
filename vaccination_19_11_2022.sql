8907fc5d735b0e9e7f2e8ffdc206e34a8d15c70d
MaximTislenko <75487198+MaximTislenko@users.noreply.github.com>
2022-11-19 10:31:37 +0300

    Update vaccination_19_11_2022.sql

--- vaccination_19_11_2022.sql
+++ vaccination_19_11_2022.sql
@@ -1,1 +1,9 @@
+USE vaccination;
 
+CREATE TABLE vaccines (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Id_препарата",
+  name_vaccine_rus VARCHAR(50) NOT NULL COMMENT "Русское именование вакцины",
+  name_vaccine_eng VARCHAR(50) NOT NULL COMMENT "Английское именование вакцины",
+  id_vac_dis_obligatory_01 VARCHAR(50) NOT NULL COMMENT "Обязательный компонент вакцины_01",
+  
+) COMMENT "Вакцинные препараты";
